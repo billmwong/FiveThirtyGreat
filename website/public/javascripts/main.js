d535a58f2999c5127fa9829e78830eac64b19e74
Bill Wong <billmwong@gmail.com>
2016-05-01 17:22:07 -0400

    ball radius support

--- website/public/javascripts/main.js
+++ website/public/javascripts/main.js
@@ -10,10 +10,12 @@ function drawChart() {
   data.addColumn('number', 'X pos');
   data.addColumn('number', 'Y pos');
   data.addColumn('string', 'Team');
-  data.addColumn('number', 'poop');
+  data.addColumn('number', 'radius');
   $.getJSON("GSWvsCLE.json", function(rawData) {
     console.log(rawData);
     var chartData = [];
+    // Make a dummy ball size so that google charts scales the rest of the sizes correctly
+    chartData.push(['Ball', 100, 0, 0, 'Ball', 150]);
 
     var firstEventMoments = rawData['events'][1]['moments'];
     // Loop through each moment
@@ -23,24 +25,24 @@ function drawChart() {
 
       chartTime = i + 100;
 
-      chartData.push(['Ball', chartTime, positionsArray[0][2], positionsArray[0][3], 'Ball', 1]);
-      chartData.push(['James', chartTime, positionsArray[1][2], positionsArray[1][3], 'CLE', 10]);
-      chartData.push(['Smith', chartTime, positionsArray[2][2], positionsArray[2][3], 'CLE', 100]);
+      chartData.push(['Ball', chartTime, positionsArray[0][2], positionsArray[0][3], 'Ball', positionsArray[0][4]]);
+      chartData.push(['James', chartTime, positionsArray[1][2], positionsArray[1][3], 'CLE', 15]);
+      chartData.push(['Smith', chartTime, positionsArray[2][2], positionsArray[2][3], 'CLE', 15]);
       chartData.push(['Love', chartTime, positionsArray[3][2], positionsArray[3][3], 'CLE', 15]);
-      chartData.push(['Irving', chartTime, positionsArray[4][2], positionsArray[4][3], 'CLE', 50]);
-      chartData.push(['Mozgov', chartTime, positionsArray[5][2], positionsArray[5][3], 'CLE', 23]);
-      chartData.push(['Bogut', chartTime, positionsArray[6][2], positionsArray[6][3], 'GSW', 70]);
-      chartData.push(['Curry', chartTime, positionsArray[7][2], positionsArray[7][3], 'GSW', 230]);
-      chartData.push(['Thompson', chartTime, positionsArray[8][2], positionsArray[8][3], 'GSW', 17]);
-      chartData.push(['Green', chartTime, positionsArray[9][2], positionsArray[9][3], 'GSW', 90]);
-      chartData.push(['Barnes', chartTime, positionsArray[10][2], positionsArray[10][3], 'GSW', 150]);
+      chartData.push(['Irving', chartTime, positionsArray[4][2], positionsArray[4][3], 'CLE', 15]);
+      chartData.push(['Mozgov', chartTime, positionsArray[5][2], positionsArray[5][3], 'CLE', 15]);
+      chartData.push(['Bogut', chartTime, positionsArray[6][2], positionsArray[6][3], 'GSW', 15]);
+      chartData.push(['Curry', chartTime, positionsArray[7][2], positionsArray[7][3], 'GSW', 15]);
+      chartData.push(['Thompson', chartTime, positionsArray[8][2], positionsArray[8][3], 'GSW', 15]);
+      chartData.push(['Green', chartTime, positionsArray[9][2], positionsArray[9][3], 'GSW', 15]);
+      chartData.push(['Barnes', chartTime, positionsArray[10][2], positionsArray[10][3], 'GSW', 15]);
     }
     // console.log(chartData);
     data.addRows(chartData);
     var chart = new google.visualization.MotionChart(document.getElementById('chart_div'));
 
     var options = {};
-    options['state'] = '{"nonSelectedAlpha":1,"xZoomedDataMin":2.04248,"iconType":"BUBBLE","yAxisOption":"3","xLambda":1,"orderedByY":false,"playDuration":6327.777777777781,"duration":{"multiplier":1,"timeUnit":"Y"},"yZoomedDataMax":45.4898,"yZoomedDataMin":3.37126,"xZoomedIn":false,"uniColorForNonSelected":false,"time":"0100","yZoomedIn":false,"yLambda":1,"showTrails":false,"dimensions":{"iconDimensions":["dim0"]},"xZoomedDataMax":73.22737,"iconKeySettings":[{"key":{"dim0":"Bogut"}},{"key":{"dim0":"Smith"}},{"key":{"dim0":"Barnes"}},{"key":{"dim0":"Green"}},{"key":{"dim0":"Thompson"}},{"key":{"dim0":"James"}},{"key":{"dim0":"Mozgov"}},{"key":{"dim0":"Curry"}},{"key":{"dim0":"Love"}},{"key":{"dim0":"Irving"}}],"xAxisOption":"2","colorOption":"4","orderedByX":false,"sizeOption":"_UNISIZE"}';
+    options['state'] = '{"nonSelectedAlpha":1,"xZoomedDataMin":0,"iconType":"BUBBLE","yAxisOption":"3","xLambda":1,"orderedByY":false,"playDuration":6327.777777777781,"duration":{"multiplier":1,"timeUnit":"Y"},"yZoomedDataMax":50,"yZoomedDataMin":0,"xZoomedIn":false,"uniColorForNonSelected":false,"time":"0100","yZoomedIn":false,"yLambda":1,"showTrails":false,"dimensions":{"iconDimensions":["dim0"]},"xZoomedDataMax":94,"iconKeySettings":[{"key":{"dim0":"Bogut"}},{"key":{"dim0":"Smith"}},{"key":{"dim0":"Barnes"}},{"key":{"dim0":"Green"}},{"key":{"dim0":"Thompson"}},{"key":{"dim0":"James"}},{"key":{"dim0":"Mozgov"}},{"key":{"dim0":"Curry"}},{"key":{"dim0":"Love"}},{"key":{"dim0":"Irving"}}],"xAxisOption":"2","colorOption":"4","orderedByX":false,"sizeOption":"5"}';
     options['width'] = 1000;
     options['height'] = 500;
     options['backgroundColor'] = 'transparent';
