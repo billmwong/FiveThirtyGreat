d836d6565e91ab0d16fbe9307d4659be43e05993
Bill Wong <billmwong@gmail.com>
2016-05-01 18:20:24 -0400

    cleaning, comments

--- website/public/javascripts/main.js
+++ website/public/javascripts/main.js
@@ -4,6 +4,7 @@ google.setOnLoadCallback(drawChart);
 google.load("visualization", "1", {packages:["motionchart"]});
 
 function drawChart() {
+  // Initialize the data columns
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Player');
   data.addColumn('number', 'Year');
@@ -11,6 +12,8 @@ function drawChart() {
   data.addColumn('number', 'Y pos');
   data.addColumn('string', 'Team');
   data.addColumn('number', 'radius');
+
+  // Load the json
   $.getJSON("GSWvsCLE.json", function(rawData) {
     console.log(rawData);
     var chartData = [];
@@ -37,10 +40,10 @@ function drawChart() {
       chartData.push(['Green', chartTime, positionsArray[9][2], positionsArray[9][3], 'GSW', 15]);
       chartData.push(['Barnes', chartTime, positionsArray[10][2], positionsArray[10][3], 'GSW', 15]);
     }
-    // console.log(chartData);
     data.addRows(chartData);
     var chart = new google.visualization.MotionChart(document.getElementById('chart_div'));
 
+    // Set the chart options
     var options = {};
     options['state'] = '{"nonSelectedAlpha":1,"xZoomedDataMin":0,"iconType":"BUBBLE","yAxisOption":"3","xLambda":1,"orderedByY":false,"playDuration":6327.777777777781,"duration":{"multiplier":1,"timeUnit":"Y"},"yZoomedDataMax":50,"yZoomedDataMin":0,"xZoomedIn":false,"uniColorForNonSelected":false,"time":"0100","yZoomedIn":false,"yLambda":1,"showTrails":false,"dimensions":{"iconDimensions":["dim0"]},"xZoomedDataMax":94,"iconKeySettings":[{"key":{"dim0":"Bogut"}},{"key":{"dim0":"Smith"}},{"key":{"dim0":"Barnes"}},{"key":{"dim0":"Green"}},{"key":{"dim0":"Thompson"}},{"key":{"dim0":"James"}},{"key":{"dim0":"Mozgov"}},{"key":{"dim0":"Curry"}},{"key":{"dim0":"Love"}},{"key":{"dim0":"Irving"}}],"xAxisOption":"2","colorOption":"4","orderedByX":false,"sizeOption":"5"}';
     options['width'] = 1000;
@@ -52,34 +55,20 @@ function drawChart() {
     options['showXScalePicker'] = false;
     options['showyScalePicker'] = false;
 
-    function placeMarker(dataTable) {
-      console.log('placemarker called');
-
-      // var cli = this.getChartLayoutInterface();
-      // var chartArea = cli.getChartAreaBoundingBox();
-
-      // console.log(dataTable.getValue(0, 3));
-      // "Zombies" is element #5.
-      // document.querySelector('.overlay-marker').style.top = Math.floor(cli.getYLocation(dataTable.getValue(0, 3))) - 50 + "px";
-      // document.querySelector('.overlay-marker').style.left = Math.floor(cli.getXLocation(dataTable.getValue(0, 2))) - 10 + "px";
-      // document.querySelector('.overlay-marker').style.top = "100px";
-      // document.querySelector('.overlay-marker').style.left = "15px";
-
+    function onChartReady(dataTable) {
       // Check every 100 ms what time step the chart is at and update the overlay text
       window.setInterval(function(){
         var rawTime = JSON.parse(this.getState()).time;
+        // Only display the first four digits
         var currentTime = rawTime.slice(0,4);
         $overlay.text("Time: "+currentTime);
       }.bind(this), 100);
     }
 
-    // function onStateChange(dataTable) {
-    //   console.log('state changed!');
-    //   console.log(this.getState());
-    // }
+    // Call onChartReady when the chart is ready
+    google.visualization.events.addListener(chart, 'ready', onChartReady.bind(chart, data));
 
-    google.visualization.events.addListener(chart, 'ready', placeMarker.bind(chart, data));
-    // google.visualization.events.addListener(chart, 'statechange', onStateChange.bind(chart, data));
+    // Draw the chart
     chart.draw(data, options);
   });
 
