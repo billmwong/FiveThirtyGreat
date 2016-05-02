52f7e69db73477059cbf672055318106d1b4fc43
acejang1994 <acejang1994@gmail.com>
2016-05-02 18:13:33 -0400

    part of final shot predictor

--- shotpredictor.py
+++ shotpredictor.py
@@ -25,61 +25,65 @@ def build_url(name, season):
     url = "http://stats.nba.com/stats/shotchartdetail?Period=0&VsConference=&LeagueID=00&LastNGames=0&TeamID=0&Position=&Location=&Outcome=&ContextMeasure=FGA&DateFrom=&StartPeriod=&DateTo=&OpponentTeamID=0&ContextFilter=&RangeType=&Season="+season+"&AheadBehind=&PlayerID="+ p_id+"&EndRange=&VsDivision=&PointDiff=&RookieYear=&GameSegment=&Month=0&ClutchTime=&StartRange=&EndPeriod=&SeasonType=Regular+Season&SeasonSegment=&GameID="
     return url
 
-def combineData(api, web):
-    web["TimeInt"] = web["Time"].apply(lambda x: int(x.replace(":", "")))    
-    web.sort_values(["Game Date", "Q", "TimeInt"],ascending=[True, True, False])
-    web.reset_index(drop=True, inplace =True)
-    return pd.concat([api, web[["Shot Dist.","Opp.", "Drib.", "Shot Clock", "Touch Time","Defender", "Def Dist."]]], axis=1)
 
-def transform_combined(data):
+def shot(s_type):
+    if "jump shot" in s_type.lower():
+        return "jump"
+    elif "layup shot" in s_type.lower():
+        return "layup"
+    else:
+        return "else"
     
-    action_type = pd.get_dummies(data.ACTION_TYPE)
-    period = pd.get_dummies(data.PERIOD)
-    shot_type = pd.get_dummies(data.SHOT_TYPE)
-    shot_zone_basic = pd.get_dummies(data.SHOT_ZONE_BASIC)
-    shot_zone_area = pd.get_dummies(data.SHOT_ZONE_AREA)
-    shot_zone_range = pd.get_dummies(data.SHOT_ZONE_RANGE)
-
-    shot_dist = data["Shot Dist."].apply(lambda x : x.replace("ft.", ""))
-    # d_rating = data["Opp."].apply(lambda x: defensive_rating[x])
-#     defender = pd.get_dummies(data["Defender"])
+def shot_dist(dist):
+    if dist < 8:
+        return "less than 8"
+    elif dist < 16:
+        return "8-16"
+    elif dist < 24:
+        return "16-24"
+    else:
+        return "24+"
     
-    new_shot_chart = pd.concat([ action_type, period, shot_type, shot_zone_basic, shot_zone_area, shot_zone_range, 
-                                  data["Shot Clock"],data["Touch Time"],
-                                data["Drib."],data["Def Dist."],
-                                data.SHOT_MADE_FLAG], axis=1)
-    return new_shot_chart
+def def_dist(dist):
+    if dist < 2:
+        return "0-2"
+    elif dist < 4:
+        return "2-4"
+    elif dist < 6:
+        return "4-6"
+    else:
+        return "6+"
 
+def transform_web(data):
+ 
+    shot_type = pd.get_dummies(data["Shot Type"].apply(shot))
+    data["Shot Dist."] = data["Shot Dist."].apply(lambda x : float(x.replace("ft.", "")))
+    shot_clock = data["Shot Clock"].apply(lambda x: float(x))
+    touch_time = data["Touch Time"].apply(lambda x: float(x))
+    drib = data["Drib."].apply(lambda x: int(x))
+    data["Def Dist."] = data["Def Dist."].apply(lambda x: float(x))
 
-def testLogLoss_combined(transformed, model):
-    predictors = transformed.columns[:-1]
-    X_train, X_test, y_train, y_test = train_test_split(transformed, transformed.SHOT_MADE_FLAG, train_size=.5)
-    print predictors
-    model.fit(X_train[predictors], y_train)
+    def_dist_c = pd.get_dummies(data["Def Dist."].apply(def_dist))
+
     
-    print "score",model.score(X_test[predictors], y_test)
-    predicted = np.array(model.predict_proba(X_test[predictors]))
-    print "Log_loss", log_loss(y_test, predicted)
+    shot_dist_c = pd.get_dummies(data["Shot Dist."].apply(shot_dist))
+    
+    con = [shot_type, shot_clock, touch_time, drib, data["Def Dist."],def_dist_c, shot_dist_c, (data["Made?"]=="Yes").astype(int)]
+    new_shot_chart = pd.concat(con , axis=1)
+
+    pred = ['16-24', '24+', '8-16', 'less than 8', 'else', 'jump', 'layup', 'Def Dist.', "Made?"]
+
+    return new_shot_chart[pred]
+
 
 def predictor(athlete, season):
-	# athlete = "stephen curry"
-	# season = "2014-15"
-	web_df = webscrape.getData(athlete, season[:-3])
-	# api_df = create_dataFrame(build_url(athlete, season))
-	# combined = combineData(api_df, web_df)
-	# transformed_combined = transform_combined(combined)
-	web_df = web_df[["Shot Dist.", "Made?"]]
-	web_df["Shot Dist."] = web_df["Shot Dist."].apply(lambda x : x.replace("ft.", ""))
-	web_df["Made?"] = web_df["Made?"].apply(lambda x: 1 if x == "Yes" else 0)
-	
-	logistic = LogisticRegression()
-	# predictors = transformed_combined.columns[:-1]
-	# logistic.fit(transformed_combined[predictors], transformed_combined.SHOT_MADE_FLAG)
-	
-	predictors = web_df.columns[:-1]
-	logistic.fit(web_df[predictors], web_df["Made?"])
 
-	return logistic
+    web_df = webscrape.getData(athlete, season)
+    transformed_web = transform_web(web_df)
+    logistic = LogisticRegression()
 
+    predictors = transformed_web.columns[:-1]
 
-
+    logistic.fit(transformed_web[predictors], transformed_web["Made?"])
+    print predictors
+    return logistic
